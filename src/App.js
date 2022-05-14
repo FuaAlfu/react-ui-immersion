d40e62ce3107a11a2421a35c28293be4d0f8fefd
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-05-14 17:22:32 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Test from './components/Test';
 import Pages from './pages/Pages';
-
+import Catagory from 'components/Catagory';
 function App() {
   return (
     <div className="App">
        <Test />
+       <Catagory/>
        <Pages />
     </div>
   );
--- src/components/Catagory.js
+++ src/components/Catagory.js
@@ -1,11 +1,50 @@
 import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
-import {GiNoodles, GiChopSticks} from 'react-icons/gi';
+import {GiNoodles, GiChopsticks} from 'react-icons/gi';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 function Category(){
     return(
-        <div>Category</div>
+        <List>
+           <NavLink to={'./cuisine/Italian'}>
+              <div className="row">
+                 <FaPizzaSlice/>
+                 <h4>Italian..</h4>
+              </div>
+              {/** end of  */}
+           </NavLink>
+              
+           <NavLink to={'./cuisine/Ameracano'}>
+              <div className="row">
+                 <FaHamburger/>
+                 <h4>Americano..</h4>
+              </div>
+              {/** end of  */}
+           </NavLink>
+
+           <NavLink to={'./cuisine/Thai'}>
+              <div className="row">
+                 <GiNoodles/>
+                 <h4>Thai..</h4>
+              </div>
+              {/** end of  */}
+           </NavLink>
+
+           <NavLink to={'./cuisine/Japanese'}>
+              <div className="row">
+                 <GiChopsticks/>
+                 <h4>Japanese..</h4>
+              </div>
+              {/** end of  */}
+           </NavLink>
+        </List>
         )
 }
+
+const List = styled.div`
+ display: flex;
+ justify-content: center;
+ margin: 2rem 0rem;
+`;
 
 export default Category;
